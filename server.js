7db3604c7e8b022bf7037bec8a3a973777f5291a
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-27 07:56:57 -0500

    added starter code for server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+//Packages
+const express = require("express");
+const mysql2 = require("mysql2");
+
+const PORT = process.env.PORT || 3001;
+//Initialize our app saving it to the express method.
+const app = express();
+
+//Express Middleware handles data parsing
+app.use(express.urlencoded ({extended: false}));
+app.use(express.json());
+
+//Connect to a database
+
+
+//GET/POST/DELETE Routes
+
+//Listen on PORT 
+app.listen(PORT, () => {console.log(`Serving running on PORT ${PORT}`)});
