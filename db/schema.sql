8659a3ff83b0d9ffc4582ec2eab0ffe24ecf55b5
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-27 19:13:02 -0500

    added last two tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,6 @@ DROP DATABASE IF EXISTS company_db
 -- Creates the "company_db" database --
 CREATE DATABASE company_db
 
--- Makes it so all of the following code will affect company_db --
-USE company_db
-
 -- Create 3 tables for Department, Role, and Employee 
 
 -- Department
@@ -13,12 +10,10 @@ USE company_db
 
 CREATE TABLE department (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
-  id INT NOT NULL,
+  id INT PRIMARY KEY,
   -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
-
-
 
 -- role
 -- id: INT PRIMARY KEY
@@ -26,9 +21,34 @@ CREATE TABLE department (
 -- salary: DECIMAL to hold role salary
 -- department_id: INT to hold reference to department role belongs to
 
+CREATE TABLE role (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT PRIMARY KEY,
+  -- Makes a string column called "name" which cannot contain null --
+  title VARCHAR(30) NOT NULL
+  salary DECIMAL NOT NULL
+  department_id: INT NOT NULL
+);
+
 -- employee
 -- id: INT PRIMARY KEY
 -- first_name: VARCHAR(30) to hold employee first name
 -- last_name: VARCHAR(30) to hold employee last name
 -- role_id: INT to hold reference to employee role
 -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+
+CREATE TABLE employee (
+  -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
+  id INT PRIMARY KEY,
+  -- Makes a string column called "name" which cannot contain null --
+  first_name VARCHAR(30) NOT NULL
+  last_name VARCHAR(30) NOT NULL
+  role_id: INT
+  manager_id: INT
+);
+
+-- Makes it so all of the following code will affect company_db --
+USE company_db
+
+
+
