166c2fe3bec2764b05b5b75ad6300b36258c95cb
Cristian Vargas <cristianvargas@Cristians-MacBook-Air.local>
2022-06-28 08:39:10 -0500

    added function to begin inquirer prompts addPrompts()

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-//Packages
+//Required Packages
 const express = require("express");
 const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
 
 const PORT = process.env.PORT || 3001;
+
 //Initialize our app saving it to the express method.
 const app = express();
 
@@ -26,8 +27,55 @@ connection.connect(function(err){
     if (err) throw err;
     //start is the function which will begin to run the inquirer prompts beginning with
     //the list "what would you like to do?""
-    start();
+    addPrompts();
 });
+
+//Function for inquirer prompts below
+function addPrompts () {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all Employees",
+            "Add an Employee",
+            "Update an Employee Role",
+            "View all Roles",
+            "Add a Role",
+            "View all Departments",
+            "Add a Department",
+            "Quit"
+        ]
+    })
+    .then((answer) => {
+        //Destructures choices 
+        const { choices } = answer
+        if (choices === "View all Employees") {
+            viewEmployees();
+        }
+        if (choices === "Add an Employee") {
+            addEmployee();
+        }
+        if (choices === "Update an Employee Role") {
+            updateEmployeeRole();
+        }
+        if (choices === "View all Roles") {
+            viewRoles();
+        }
+        if (choices === "Add a Role") {
+            addRole();
+        }
+        if (choices === "View all Departments") {
+            viewDepartments();
+        }
+        if (choices === "Add a Department") {
+            addDepartment();
+        }
+        if (choices === "Quit") {
+            connection.end();
+        };
+    })
+}
 
 //GET/POST/DELETE Routes
 
